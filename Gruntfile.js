a2bc114c8f0b149acc0f5b41edd121c4e94f85a2
ryleybridges <rye.brid@gmail.com>
2019-09-17 11:50:09 +1200

    Began setting grunt up

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,29 @@
-module.exports = function(grunt) {
-
+module.exports = function(grunt){
 	grunt.initConfig({
-
+		watch: {
+			js: {
+				files: [‘js/*.js’, ‘!js/*.min.js’],
+				tasks: [‘jshint’, ‘uglify']
+			}
+		},
+		jshint: {
+			files: [‘js/*.js’, ‘!js/*.min.js’],
+			options: {
+				esversion: 6
+			}
+		},
+		uglify: {
+      my_target: {
+        files: {
+          'js/server.min.js': ['js/server.js']
+        }
+      }
+    }
 	});
 
-}
+	grunt.loadNpmTasks(‘grunt-contrib-watch’);
+	grunt.loadNpmTasks(‘grunt-contrib-jshint’);
+	grunt.loadNpmTasks(‘grunt-contrib-uglify-es’);
+
+	grunt.registerTask('runWatch', ['watch'])
+};
