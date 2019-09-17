a56f79ac8b4aa63d08dbeec5801163d33a875705
ryleybridges <rye.brid@gmail.com>
2019-09-17 14:07:09 +1200

    Set up back end grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,29 +1,37 @@
-module.exports = function(grunt){
-	grunt.initConfig({
-		watch: {
-			js: {
-				files: [‘js/*.js’, ‘!js/*.min.js’],
-				tasks: [‘jshint’, ‘uglify']
-			}
-		},
-		jshint: {
-			files: [‘js/*.js’, ‘!js/*.min.js’],
-			options: {
-				esversion: 6
-			}
-		},
-		uglify: {
-      my_target: {
-        files: {
-          'js/server.min.js': ['js/server.js']
-        }
-      }
-    }
-	});
+module.exports = function (grunt){
 
-	grunt.loadNpmTasks(‘grunt-contrib-watch’);
-	grunt.loadNpmTasks(‘grunt-contrib-jshint’);
-	grunt.loadNpmTasks(‘grunt-contrib-uglify-es’);
+    grunt.initConfig({
+      jshint: {
+          files: ['server.js'],
+          options: {
+            esversion: 6,
+              globals:{
+                  jQuery: true
+              }
+          }
+      },
+      watch: {
+          jsval: {
+            files: ['server.js'],
+            tasks: ['jshint']
+          }
+      },
+			uglify: {
+      	my_target: {
+        	files: {
+          	'server.min.js': ['server.js']
+        	},
+        	options: {
+            	esversion: 6
+        	}
+      	}
+			}
+    });
 
-	grunt.registerTask('runWatch', ['watch'])
-};
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-uglify-es');
+
+
+  grunt.registerTask('runWatch', ['watch']);
+}
