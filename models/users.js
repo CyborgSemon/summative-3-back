4fa1377ae49f492041c590f3fb0d846bc93553cd
ryleybridges <rye.brid@gmail.com>
2019-09-17 15:26:55 +1200

    Updated model for users

--- models/users.js
+++ models/users.js
@@ -1,3 +1,14 @@
 const mongoose = require('mongoose');
 
-const U
+const usersSchema = new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    name: String,
+    address: String,
+    username: String,
+    password: String,
+    email: String,
+    dob: Date,
+    registerDate: Date
+});
+
+module.exports = mongoose.model('Users', usersSchema);
