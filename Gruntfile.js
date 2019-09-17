82aa724951e80ab484299d8980e6ed84b07cddb3
ryleybridges <rye.brid@gmail.com>
2019-09-17 14:46:34 +1200

    Set up server

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,37 +1,37 @@
 module.exports = function (grunt){
 
-    grunt.initConfig({
-      jshint: {
-          files: ['server.js'],
-          options: {
-            esversion: 6,
-              globals:{
-                  jQuery: true
-              }
-          }
-      },
-      watch: {
-          jsval: {
-            files: ['server.js'],
-            tasks: ['jshint']
-          }
-      },
-			uglify: {
-      	my_target: {
-        	files: {
-          	'server.min.js': ['server.js']
-        	},
-        	options: {
-            	esversion: 6
-        	}
-      	}
+	grunt.initConfig({
+		jshint: {
+			files: ['server.js'],
+			options: {
+				esversion: 6,
+				globals:{
+					jQuery: true
+				}
 			}
-    });
+		},
+		watch: {
+			jsval: {
+				files: ['server.js'],
+				tasks: ['jshint']
+			}
+		},
+		uglify: {
+			my_target: {
+				files: {
+					'server.min.js': ['server.js']
+				},
+				options: {
+					esversion: 6
+				}
+			}
+		}
+	});
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-uglify-es');
 
 
-  grunt.registerTask('runWatch', ['watch']);
+	grunt.registerTask('runWatch', ['watch']);
 }
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require(`body-parser`);
+const cors = require(`cors`);
+const mongoose = require(`mongoose`);
+const bcrypt = require(`bcryptjs`);
+
+const config = require(`./config.json`);
+
+mongoose.connect(`mongodb+srv://${config.MONGO_USERNAME}:${config.MONGO_PASSWORD}@${config.CLUSTER_NAME}.mongodb.net/${config.TABLE_NAME}?retryWrites=true&w=majority`, {useNewUrlParser: true});
+
+    const db = mongoose.connection;
+    db.on(`error`, console.error.bind(console, `connection error:`));
+    db.once(`open`, function(){
+        console.log(`we are connected mongo db`);
+    });
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlenconded({extended:false}));
+    app.use(cors());
+
+    app.use(function(req, res, next){
+        console.log(`${req.method} request for ${req.url}`);
+        next();
+    });
+
+    app.get(`/`, function(req, res){
+        res.send(`Welcome to our Pop Culture Merchandise Niche Market App`);
+    });
+
+    app.listen(port, () => {
+        console.log(`application is running on port ${port}`);
+    });
